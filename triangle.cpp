9151f58e1e7ed2d163fd91670c24e173e11ea9da
larsonee <antosha.kravchenko@gmail.com>
2014-10-28 21:07:20 +0200

    rework rotate vector method

--- triangle.cpp
+++ triangle.cpp
@@ -4,7 +4,8 @@ int 	Triangle::RIGHT_EDGE 	= 0;
 int 	Triangle::BOTTOM_EDGE 	= 1;
 int 	Triangle::LEFT_EDGE		= 2;
 float 	Triangle::ROTATE_ANGLE 	= 30;
-float 	Triangle::EGDE_LENGTH 	= -0.99;//very bad
+float 	Triangle::EGDE_LENGTH 	= -0.3;//very bad
+float 	Triangle::EDGE_MULT     = 1.15;
 double	Triangle::PI 			=3.141592653589793238463;
 
 
@@ -57,7 +58,7 @@ void Triangle::init_edges()
 	for(int i = 0, next = 1, angle = 0; i < 3; i++, angle += 120, next++) //must be 120 degree to get right triangle
 	{
 		Point *v = new Point();
-		rotate_point(v, angle);
+		rotate_point(v, center, angle);
 		bounds[i] = v;
 	}
 
@@ -72,19 +73,31 @@ void Triangle::init_edges()
 	draw_doodle();
 }
 
-Point* Triangle::rotate_point(Point* p, float angle)
+Point* Triangle::rotate_point(Point* p, Point* anchor, float angle)
 {
 	float vx = 0; //can be changed to rotate entire rectangle
 	float vy = Triangle::EGDE_LENGTH;
 	//p->x can be mult by -1 to change pivot point of rotate
 	p->x = -(vx*cos(to_radians(angle)) - vy*sin(to_radians(angle)));
 	p->y =  vx*sin(to_radians(angle)) + vy*cos(to_radians(angle));
+	p->x += anchor->x;
+	p->y += anchor->y;
 //	p->draw();
 	return p;
 }
 
-Line* Triangle::rotate_vector(Line* vector, float angle)
+Line* Triangle::rotate_vector(Line* vector, Point* center,float angle)
 {
+	static Point *anchor = new Point();
+
+	anchor->x = vector->a->x;
+	anchor->y = vector->a->y;
+
+	vector->b->x -= anchor->x;//0
+	vector->b->y -= anchor->y;//0
+	vector->a->x -= anchor->x;
+	vector->a->y -= anchor->y;
+
 	float vx = vector->b->x;
 	float vy = vector->b->y;
 
@@ -97,14 +110,18 @@ Line* Triangle::rotate_vector(Line* vector, float angle)
 //	vector->a->x = (vx*cos(to_radians(angle)) - vy*sin(to_radians(angle)));
 //	vector->a->y =   (vx*sin(to_radians(angle)) + vy*cos(to_radians(angle)));
 
-//	vector->draw();
+	vector->b->x += anchor->x;
+	vector->b->y += anchor->y;
+
+	vector->a->x += anchor->x;
+	vector->a->y += anchor->y;
 	return vector;
 }
 
 void Triangle::draw_doodle()
 {
 	float rotate_angle = -1;
-	for(int a = 0; a < 150; a++, rotate_angle-= 0.005)
+	for(int a = 0; a < 100; a++, rotate_angle-= 0.005)
 	{
 		for(int i = 0; i < 3; i++)
 		{
@@ -123,7 +140,7 @@ void Triangle::draw_doodle()
 
 	//[1]
 			// can be negative with vx value in rotate point line:81
-			rotate_vector(vector, rotate_angle);
+			rotate_vector(vector, this->center, rotate_angle);
 
 	//[2]
 			Point *inters_point = new Point();
@@ -139,7 +156,7 @@ void Triangle::draw_doodle()
 			edges[cur]->draw();
 
 //		sleep(2);
-		usleep(50000);
+		usleep(10000);
 		}
 	}
 }
